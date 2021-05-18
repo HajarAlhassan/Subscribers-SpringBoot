ed42ebd2e81fd51fc53f61c21622664c618f29e9
Hajar Alhassan <79155491+HajarAlhassan@users.noreply.github.com>
2021-05-18 14:43:40 -0400

    Add files via upload

---
+++ SubscriberList/src/main/java/com/tts/SubscriberList/repository/SubscriberRepository.java
@@ -0,0 +1,10 @@
+package com.tts.SubscriberList.repository;
+
+import com.tts.SubscriberList.model.Subscriber;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface SubscriberRepository extends CrudRepository<Subscriber,Long> {
+
+}
